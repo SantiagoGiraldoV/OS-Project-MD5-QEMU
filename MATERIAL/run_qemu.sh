0620f78e414fe77cbba38dc3bb65d190b01f4920
SantiagoGiraldoV <107322851+SantiagoGiraldoV@users.noreply.github.com>
2024-07-21 15:56:13 -0500

    Delete MATERIAL directory

--- MATERIAL/run_qemu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-qemu-system-aarch64 -cpu cortex-a53 \
-                    -machine virt \
-                    -m 2048M \
-                    -kernel ~/Downloads/kernel-qemu-4.19.50-buster \
-                    -append "root=/dev/vda2 panic=1" \
-                    -drive file=~/Downloads/raspios-bookworm-arm64-lite.qcow2,format=raw \
-                    -serial stdio
